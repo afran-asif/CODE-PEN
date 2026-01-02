dc55a4f70cd16b177eacc8dcfa7de49ef0b63768
asif is here <asifafran24@gmail.com>
2026-01-02 21:16:59 +0600

    split CodeMirror and FontAwesome into separate JS files, reducing main bundle size

--- src/Components/Editor.jsx
+++ src/Components/Editor.jsx
@@ -1,59 +1,58 @@
-// Editor.jsx
 import React, { useEffect, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Editor({ displayName, value, language, onChange }) {
-  const [open, setOpen] = useState(true);
-  const [extensions, setExtensions] = useState([]);
+const [open, setOpen] = useState(true);
+const [extensions, setExtensions] = useState([]);
 
-  useEffect(() => {
+useEffect(() => {
     let isMounted = true;
 
     async function loadLanguage() {
-      if (language === "html") {
+    if (language === "html") {
         const { html } = await import("@codemirror/lang-html");
         if (isMounted) setExtensions([html()]);
-      } else if (language === "css") {
+    } else if (language === "css") {
         const { css } = await import("@codemirror/lang-css");
         if (isMounted) setExtensions([css()]);
-      } else if (language === "javascript") {
+    } else if (language === "javascript") {
         const { javascript } = await import("@codemirror/lang-javascript");
         if (isMounted) setExtensions([javascript()]);
-      }
+    }
     }
 
     loadLanguage();
 
     return () => {
-      isMounted = false;
+    isMounted = false;
     };
-  }, [language]);
+}, [language]);
 
-  return (
+return (
     <div className={`editor-container ${open ? "" : "collapsed"}`}>
-      <div className="editor-title">
+    <div className="editor-title">
         {displayName}
         <button
-          type="button"
-          className="expend-collapse-button"
-          onClick={() => setOpen((prev) => !prev)}
+        type="button"
+        className="expend-collapse-button"
+        onClick={() => setOpen((prev) => !prev)}
         >
-          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
         </button>
-      </div>
-
-      {open && (
-        <CodeMirror
-          value={value}
-          className="code-mirror-wrapper"
-          style={{ flexGrow: 1 }}
-          extensions={extensions}
-          theme="dark"
-          onChange={(value, viewUpdate) => onChange(value)}
-        />
-      )}
     </div>
-  );
+
+    {open && (
+        <CodeMirror
+        value={value}
+        className="code-mirror-wrapper"
+        style={{ flexGrow: 1 }}
+        extensions={extensions}
+        theme="dark"
+        onChange={(value, viewUpdate) => onChange(value)}
+        />
+    )}
+    </div>
+);
 }
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/CODE-PEN/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codemirror: ['@codemirror/lang-html', '@codemirror/lang-css', '@codemirror/lang-javascript'],
+          fontawesome: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons'],
+        }
+      }
+    }
+  }
 })
